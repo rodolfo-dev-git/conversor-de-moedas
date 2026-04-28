24f500108e458321a00f096824a67c136541f181
rodolfo-dev-git <rodolfodevgit@gmail.com>
2026-04-28 13:45:06 -0300

    Iniciando o Conversor de Moedas

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const converterButton = document.querySelector(".convert-button");
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");    // Moeda Inicial
+    const currencyValueConverted = document.querySelector(".currency-value");            // Moeda Convertida
 
     const dolarToday = 5.02;             //Valor Ficticio
 
     const convertedValue = inputCurrencyValue / dolarToday;
 
-    console.log(convertedValue);
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue);
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue);
+
 }
 
 converterButton.addEventListener("click", convertValues);
