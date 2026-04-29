2ae188ce37c6968d8408be19bfa3534246646d4b
rodolfo-dev-git <rodolfodevgit@gmail.com>
2026-04-29 18:34:37 -0300

    adicionando outras moedas

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function convertValues() {
 
     const dolarToday = 5.02;             //Valor Ficticio
     const euroToday = 5.85;             //Valor Ficticio
+    const libraToday = 6.75;             //Valor Ficticio
+    const pesoaToday = 0.0036;             //Valor Ficticio
+
+
     const bitToday = 380146;             //Valor Ficticio
 
 
@@ -27,8 +31,20 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday);
     }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday);
+    }
+    if (currencySelect.value == "pesoa") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("es-AR", {
+            style: "currency",
+            currency: "ARS"
+        }).format(inputCurrencyValue / pesoaToday);
+    }
     if (currencySelect.value == "bitcoin") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "BTC"
         }).format(inputCurrencyValue / bitToday);
@@ -50,10 +66,22 @@ function changeCurrency() {
         currencyName.innerHTML = "Dólar americano";
         currencyImage.src = "./assets/img/usa.png";
     }
+
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro";
         currencyImage.src = "./assets/img/uniao.png";
     }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra";
+        currencyImage.src = "./assets/img/libra.png";
+    }
+
+    if (currencySelect.value == "pesoa") {
+        currencyName.innerHTML = "Peso Argentino";
+        currencyImage.src = "./assets/img/argentina.png";
+    }
+
     if (currencySelect.value == "bitcoin") {
         currencyName.innerHTML = "Bitcoin";
         currencyImage.src = "./assets/img/bitcoin.png";
